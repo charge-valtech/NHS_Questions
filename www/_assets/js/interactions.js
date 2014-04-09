0cb380339a3b13ef8764adac97215f67568e8635
Henry Charge <henry.charge@valtech.co.uk>
2014-04-09 10:16:26 +0100

    Start to refactor forms to comply with http://govuk-elements.herokuapp.com/

--- www/_assets/js/interactions.js
+++ www/_assets/js/interactions.js
@@ -4,4 +4,31 @@ $(function() {
     $(this).toggleClass('triggered')
     return false;
   });
+  $(".block-label").each(function(){
+
+      // Add focus
+      $(".block-label input").focus(function() {
+        $("label[for='" + this.id + "']").addClass("add-focus");
+        }).blur(function() {
+        $("label").removeClass("add-focus");
+      });
+      // Add selected class
+      $('input:checked').parent().addClass('selected');
+
+    });
+
+    // Add/remove selected class
+    $('.block-label').find('input[type=radio], input[type=checkbox]').click(function() {
+
+      $('input:not(:checked)').parent().removeClass('selected');
+      $('input:checked').parent().addClass('selected');
+
+      // Hide open data-toggle content
+      $('.toggle-content').hide();
+
+      // Show data-toggle content
+      var target = $(this).parent().attr('data-target');
+      $('#'+target).show();
+
+    });
 });
