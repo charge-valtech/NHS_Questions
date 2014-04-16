7c80f30f1e1caf1cb01aece90eecd603c59d1013
Henry Charge <henry.charge@valtech.co.uk>
2014-04-16 10:32:23 +0100

    Push to sprint

--- sprint/_assets/js/interactions.js
+++ sprint/_assets/js/interactions.js
@@ -6,38 +6,69 @@ $(function() {
   });
   $(".block-label").each(function(){
 
-      // Add focus
-      $(".block-label input").focus(function() {
-        $("label[for='" + this.id + "']").addClass("add-focus");
-        }).blur(function() {
-        $("label").removeClass("add-focus");
-      });
-      // Add selected class
-      $('input:checked').parent().addClass('selected');
-
+    // Add focus
+    $(".block-label input").focus(function() {
+      $("label[for='" + this.id + "']").addClass("add-focus");
+      }).blur(function() {
+      $("label").removeClass("add-focus");
     });
+    // Add selected class
+    $('input:checked').parent().addClass('selected');
 
-    // Add/remove selected class
-    $('.block-label').find('input[type=radio], input[type=checkbox]').click(function() {
+  });
 
-      $('input:not(:checked)').parent().removeClass('selected');
-      $('input:checked').parent().addClass('selected');
+  $('.form-control').keyup(function(){
+    if($(this).val().length==$(this).attr("maxlength")){
+      $(this).parent().next('.form-group').find('.form-control').focus();
+    }
+  });
 
-      // Hide open data-toggle content
-      $('.toggle-content').hide();
+  // Add/remove selected class
+  $('.block-label').find('input[type=radio], input[type=checkbox]').click(function() {
 
-      // Show data-toggle content
-      var target = $(this).parent().attr('data-target');
-      $('#'+target).show();
+    $('input:not(:checked)').parent().removeClass('selected');
+    $('input:checked').parent().addClass('selected');
 
-    });
+    // Hide open data-toggle content
+    $('.toggle-content').hide();
 
-    $('.amend-answers').on('click', function() {
-      $(this).closest('.form-group').toggleClass('expanded');
-      return false;
-    });
+    // Show data-toggle content
+    var target = $(this).parent().attr('data-target');
+    $('#'+target).show();
 
-    $('.update-answers').on('click', function() {
-      $(this).closest('.form-group').toggleClass('expanded');
-    });
+  });
+
+  $('.amend-answers').on('click', function() {
+    $(this).closest('.form-group').toggleClass('expanded');
+    return false;
+  });
+
+  $('.update-answers').on('click', function() {
+    $(this).closest('.form-group').toggleClass('expanded');
+  });
+
+  // Example - Details & summary
+
+  // // Add aria support for details
+  // $('details').details();
+
+  // // If details is not supported, add class to html
+  // if (!Modernizr.details) {
+
+  //   $('html').addClass('no-details');
+
+  //   $('summary').click(function() {
+
+  //     var $details, $summary;
+  //     $summary = $(this);
+  //     $details = $summary.parent();
+
+  //     if ($details.attr('open')) {
+  //       return $details.removeAttr('open');
+  //     } else {
+  //       return $details.attr('open', 'open');
+  //     }
+
+  //   });
+  // }
 });
