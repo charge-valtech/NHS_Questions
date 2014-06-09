c17721782e8fc452e686317b0d4fdc16bee01d14
Henry Charge <henry.charge@valtech.co.uk>
2014-06-09 18:26:04 +0100

    Push to sprint

--- sprint/_assets/js/scripts.js
+++ sprint/_assets/js/scripts.js
@@ -2031,12 +2031,88 @@ if (typeof define == 'function' && typeof define.amd == 'object' && define.amd) 
 
 // ------------ Apply for vacancy mockup ------------ //
 
-  $('#saveQualification').on('click', function(){
+  $('#saveQualification').on('click', function(e){
     var $qualType    = $('#qual-type').val(),
+        $qualID      = $('#qual-type').find(":selected").attr('class'),
         $qualSubject = $('#subject-name').val()
         $qualGrade   = $('#subject-grade').val()
-        $isPredicted = $('#qual-predicted').is(':checked');
+        $isPredicted = $('#qual-predicted').is(':checked'),
+        $isPredValue = ($isPredicted ? " (Predicted)" : ""),
+        $rowHTML     = '<tr class="tr-qualRow">' +
+                          '<td class="td-qualcell">' +
+                            '<span>' + $qualSubject + '</span>' +
+                            '<input class="qual-input-edit" type="text" value="' + $qualSubject + '">' +
+                          '</td>' +
+                          '<td class="td-qualcell">' +
+                            '<span>' + $qualGrade + $isPredValue + '</span>' +
+                            '<input class="qual-input-edit" type="text" value="' + $qualGrade + '">' +
+                          '</td>' +
+                          '<td class="fake-link td-qualEdit">Edit</td>' +
+                        '</tr>',
+        $emptyTable  = '<div class="qualification-table"' + 'id="' + $qualID + '">' +
+                        '<h3 class="heading-small heading-qualType">' + $qualType + '</h3>' +
+                        '<table class="grid-3-4">' +
+                          '<colgroup>' +
+                            '<col class="t55">' +
+                            '<col class="t35">' +
+                            '<col class="t10">' +
+                            '<col>' +
+                          '</colgroup>' +
+                          '<thead>' +
+                            '<tr>' +
+                              '<th class="th-qualSubject">Subject</th>' +
+                              '<th class="th-qualGrade">Grade</th>' +
+                              '<th></th>' +
+                            '</tr>' +
+                          '</thead>' +
+                          '<tbody class="tbody-qual">' +
+                            $rowHTML +
+                          '</tbody>' +
+                        '</table>' +
+                      '</div>';
 
+    if($('.tr-qualRow').length == 0) {
+      $('.qualification-table').show().attr('id', $qualID);
+      $('.heading-qualType').html($qualType);
+      $('.tbody-qual').html($rowHTML);
+    } else {
+      $('#' + $qualID).find('.tbody-qual').append($rowHTML);
+      diffQual();
+    }
+
+    function diffQual() {
+      if($('#' + $qualID).length == 0) {
+        $('.qualifications-wrapper').append($emptyTable);
+      }
+    }
+
+    $('#subject-name').val('');
+    $('#subject-grade').val('');
+    $('#qual-predicted').prop('checked', false);
+
+    e.preventDefault();
+
+  });
+
+  $('.qualifications-wrapper').on('click', '.td-qualEdit', function(e) {
+    var $this      = $(this),
+        $qualSpans = $this.siblings().find('span'),
+        $editBoxes = $this.siblings().find('.qual-input-edit');
+
+    $this.text('Save').addClass('qualSave');
+
+    $qualSpans.hide();
+    $editBoxes.show();
+
+    e.preventDefault();
+  });
+
+  $('.qualifications-wrapper').on('click', '.qualSave', function(e) {
+    var $this = $(this);
+
+    $this.text('Edit').removeClass('qualSave');
+
+    e.preventDefault();
   });
 
 // --------------- Remove for live code -------------- //
