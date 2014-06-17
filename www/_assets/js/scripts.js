81b5c54d93210d65620166e28876e8779a828e18
Henry Charge <henry.charge@valtech.co.uk>
2014-06-17 11:47:06 +0100

    Work experience working with icons to trigger editable fields CHANGE

--- www/_assets/js/scripts.js
+++ www/_assets/js/scripts.js
@@ -2213,17 +2213,22 @@ if (typeof define == 'function' && typeof define.amd == 'object' && define.amd) 
         $historyHTML  = '<div class="work-history text">' +
                           '<div class="hgroup-small">' +
                             '<h3 class="work-editsection heading-small heading-with-border"><i class="ir icon-edit">Edit</i>' + 
+                              '<input class="form-control editable-work-input toggle-content" value="' + $workEmployer + '">' +
                               '<span class="editable-work">' + $workEmployer + '</span>' +
                               ' - ' + 
+                              '<input class="form-control editable-work-input toggle-content" value="' + $workTitle + '">' +
                               '<span class="editable-work">' + $workTitle + '</span>' +
                             '</h3>' +
-                            '<h4 class="subtitle work-editsection"><span class="editable-work"><i class="ir icon-edit">Edit</i>'+ 
+                            '<h4 class="subtitle work-editsection"><i class="ir icon-edit">Edit</i>'+ 
+                              '<input class="form-control editable-work-input toggle-content" value="' + $workFrom + '">' +
                               '<span class="editable-work">' + $workFrom + '</span>' +
                               ' - ' + 
+                              '<input class="form-control editable-work-input toggle-content" value="' + $workTo + $isCurrValue + '">' +
                               '<span class="editable-work">' + $workTo + $isCurrValue + '</span>' +
                             '</h4>' +
                           '</div>' +
-                          '<p class="copy-16 work-editsection"><i class="ir icon-edit">Edit</i>'+ 
+                          '<p class="copy-16 work-editsection"><i class="ir icon-edit next-textarea">Edit</i>'+ 
+                            '<textarea class="form-control editable-work-input toggle-content">' + $workRole + '</textarea>' +
                             '<span class="editable-work">' + $workRole + '</span>' +
                           '</p>' +
                           '<p class="copy-16"><a href="#" class="work-edit">Edit</a> or <a href="#" class="work-delete">Delete</a></p>' +
@@ -2275,9 +2280,18 @@ if (typeof define == 'function' && typeof define.amd == 'object' && define.amd) 
 
   });
 
+  $('.work-history-wrapper').on('keyup', '.editable-work-input', function() {
+    var $thisVal = $(this).val();
+
+    $(this).next('.editable-work').text($thisVal);
+  });
+
   $('.work-history-wrapper').on('click', '.work-save', function(e) {
     $(this).closest('.work-history').removeClass('edit-mode');
     $(this).text('Edit').removeClass('work-save');
+
+    $(this).closest('.work-history').find('.editing-worksection').removeClass('editing-worksection');
+    $(this).closest('.work-history').find('.icon-tick').removeClass('icon-tick').addClass('icon-edit');
 
     e.preventDefault();
   });
