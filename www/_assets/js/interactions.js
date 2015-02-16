621089dffff6cb6276eb633d518829759f30b92f
Henry Charge <henry.charge@valtech.co.uk>
2015-02-16 10:20:32 +0000

    Run fastclick on mobile only

--- www/_assets/js/interactions.js
+++ www/_assets/js/interactions.js
@@ -1,7 +1,29 @@
 $(function() {
-  var rapidClicking = FastClick;
 
-  rapidClicking.attach(document.body);
+  var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+  };
+
+  if (isMobile.matches) {
+    FastClick.attach(document.body);
+  }
 
   function isAndroid() {
     var nua = navigator.userAgent,
